c171520e3e45647fc0bee268609b7597740a56ce
AnkitMU <jhaankit1200@gmail.com>
2024-11-13 16:02:42 +0000

    spring boot project

---
+++ backend_services/userService/src/main/java/onlypans/userService/controller/UserController.java
@@ -0,0 +1,58 @@
+package onlypans.userService.controller;
+
+import onlypans.common.dtos.CreatorProfileRequest;
+import onlypans.userService.entity.User;
+import onlypans.userService.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        return new ResponseEntity<>(userService.updateUser(id, userDetails), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @PostMapping("/{userId}/upgrade")
+    public ResponseEntity<String> upgradeToCreatorProfile(
+            @PathVariable Long userId,
+            @RequestBody CreatorProfileRequest request) {
+        request.setUserId(userId);
+
+        userService.upgradeToCreatorProfile(request);
+        return ResponseEntity.ok("User upgraded to creator profile");
+    }
+}
+
